d063cdfe50154913c6d4c02a225964b3e13465b6
Lucas Rocha <ld.rocha@hotmail.com>
2021-04-09 13:22:28 -0700

    create header component and home component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
 import Head from 'next/head'
 
+import styles from './home.module.scss'
+
 
 export default function Home() {
 	return (
@@ -7,9 +9,18 @@ export default function Home() {
 		<Head>
 			<title>Home - ig.news</title>
 		</Head>
-		<h1>
-			Hello World
-		</h1>
+		<main className={styles.contentContainer}>
+			<section className={styles.hero}>
+				<span>👋 Hey, welcome</span>
+				<h1>News about <br/> the  <span>React</span> world.</h1>
+				<p>
+					Get access to all publications <br/>
+					<span>for $9.90 month</span>
+				</p>
+			</section>
+
+			<img src="/images/avatar.svg" alt="Girl coding"/>
+		</main>
 		</>
 	)
 }
