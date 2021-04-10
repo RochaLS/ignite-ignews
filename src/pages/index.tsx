212928e3975cbaba50b71d4b4bae64fcbb4071c0
Lucas Rocha <ld.rocha@hotmail.com>
2021-04-09 19:44:53 -0700

    get info from the stripe api using SSG

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,19 @@
+import { GetStaticProps } from 'next'
 import Head from 'next/head'
 import { SubscribeButton } from '../components/SubscribeButton'
+import { stripe } from '../services/stripe'
 
 import styles from './home.module.scss'
 
+interface HomeProps {
+	product: {
+		priceId: string;
+		amount: number;
+	}
+}
 
-export default function Home() {
+
+export default function Home({ product }: HomeProps) {
 	return (
 		<>
 		<Head>
@@ -16,8 +25,8 @@ export default function Home() {
 				<h1>News about <br/> the  <span>React</span> world.</h1>
 				<p>
 					Get access to all publications <br/>
-					<span>for $9.90 month</span>
-					<SubscribeButton/>
+					<span>for {product.amount} month</span>
+					<SubscribeButton priceId={product.priceId}/>
 				</p>
 			</section>
 
@@ -25,4 +34,23 @@ export default function Home() {
 		</main>
 		</>
 	)
+}
+
+export const getStaticProps: GetStaticProps =  async () => {
+
+	const price = await stripe.prices.retrieve('price_1IeVB9DGXzrZ5HlcZ3T1Lk3t')
+
+	const product = {
+		priceID: price.id,
+		amount: new Intl.NumberFormat('en-US', {
+			style: 'currency',
+			currency: 'USD'
+		}).format((price.unit_amount / 100))
+	}
+	return {
+		props: {
+			product,
+		},
+		revalidate: 60 * 60 * 24 // 24 hours
+	}
 }
